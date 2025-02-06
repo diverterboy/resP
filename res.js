5a21d092ce053771be74123db3e25407d7387be6
alideveloper-web <aliii.moradiii2024@gmail. com>
2025-02-06 13:34:13 -0800

    added restaurants list

--- res.js
+++ res.js
@@ -1,8 +1,32 @@
-console.log("ame")
+const restaurantList = document.querySelector("#restaurantList")
+const xhrRestaurant = new XMLHttpRequest()
+
+function loadRestaurants () {
+
+    const xhrRestaurant = new XMLHttpRequest()
+    xhrRestaurant.open("Get", "https://food.utotech.ir/restaurants.php", true)
+    xhrRestaurant.onload = function () {
 
 
+        if (xhrRestaurant.status === 200) {
+            const myResponse = (JSON.parse(xhrRestaurant.responseText))
+            for (let i = 0; i < myResponse.length; i++) {
+                let restaurants = myResponse[i]
+                console.log(restaurants)
+                let r = ` <li onclick="selectRestaurant('${restaurants}')">${restaurants}</li>
+           `
+
+                restaurantList.innerHTML = restaurantList.innerHTML + r
+            }
+        } else {
+            console.log(xhrRestaurant.status)
+        }
+    }
+    xhrRestaurant.send()
+
+}
 function selectRestaurant(items){
     localStorage.setItem("res",items)
     window.location.href = "index.html";
 }
-selectRestaurant()
+loadRestaurants()
